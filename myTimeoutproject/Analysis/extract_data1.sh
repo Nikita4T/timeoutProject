950af15ec80c6b4c2199ab812358c52a0cf64a79
Nikita <nikita-hess@outlook.com>
2026-08-18 22:34:11 +0200

    Add comments to extract_data1.sh for clarity
    Added comments to clarify the source of the script.

--- myTimeoutproject/Analysis/extract_data1.sh
+++ myTimeoutproject/Analysis/extract_data1.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Adapted from the "Exercise with Veins" tutorial by Lorenzo Ghiro:
+# https://ans.unibs.it/docs/veins_exercise.html
+# Modified for the data analysis of this project.
 RESULT_FOLDER="$RESULT_DIR/result_by_timeout/timeout_0-20"
 for sca in $(ls $RESULT_FOLDER/*sca) 
 do
